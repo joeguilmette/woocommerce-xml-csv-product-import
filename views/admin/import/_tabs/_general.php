fa08c3d358c7e19d4b38c277e5033b23bef8a110
Joe Guilmette <joe.guilmette@gmail.com>
2016-08-25 19:42:48 +0900

    copy and style changes

--- views/admin/import/_tabs/_general.php
+++ views/admin/import/_tabs/_general.php
@@ -1,6 +1,6 @@
 <div class="panel woocommerce_options_panel" id="general_product_data">
 	<div class="woo-add-on-free-edition-notice upgrade_template">
-		<a href="http://www.wpallimport.com/upgrade-to-pro/?utm_source=free-plugin&utm_medium=in-plugin&utm_campaign=woocommerce" target="_blank" class="upgrade_woo_link"><?php _e('Upgrade to the Pro edition of WP All Import and the WooCommerce add-on to import to Variable, Affiliate, and Grouped products.', 'wpai_woocommerce_addon_plugin');?></a>
+		<a href="http://www.wpallimport.com/upgrade-to-pro/?utm_source=free-plugin&utm_medium=in-plugin&utm_campaign=woocommerce" target="_blank" class="upgrade_woo_link"><?php _e('Upgrade to the Pro edition of WP All Import and the WooCommerce Add-On to Import to Variable, Affiliate, and Grouped Products.', 'wpai_woocommerce_addon_plugin');?></a>
 		<p><?php _e('If you already own it, remove the free edition and install the Pro edition.', 'wp_all_import_plugin'); ?></p>
 	</div>	
 	<div class="options_group hide_if_grouped">
